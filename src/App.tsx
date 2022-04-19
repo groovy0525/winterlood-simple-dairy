b0461b596dd53bc469a8f64dbb08131430490738
groovy0525 <60678333+groovy0525@users.noreply.github.com>
2022-04-19 22:39:51 +0900

    React에서 배열 사용하기 1 - 리스트 렌더링

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,37 @@
-import "./App.css"
 import DiaryEditor from "./components/DiaryEditor"
+import DiaryList from "./components/DiaryList"
+import { Diary } from "./types"
+
+const dummyList: Diary[] = [
+  {
+    id: 1,
+    author: "이정환",
+    content: "하이 1",
+    emotion: 5,
+    created_date: new Date().getTime(),
+  },
+  {
+    id: 2,
+    author: "홍길동",
+    content: "하이 2",
+    emotion: 2,
+    created_date: new Date().getTime(),
+  },
+  {
+    id: 3,
+    author: "아무개",
+    content: "하이 3",
+    emotion: 1,
+    created_date: new Date().getTime(),
+  },
+]
 
 function App() {
   return (
-    <div className="App">
+    <>
       <DiaryEditor />
-    </div>
+      <DiaryList diaryList={dummyList} />
+    </>
   )
 }
 
